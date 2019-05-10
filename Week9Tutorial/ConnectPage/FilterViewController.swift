5661602e96976eb9cb375b847c15c5f00555efd7
Ziyi Deng <ziyideng@dyn-118-138-81-86.its.monash.edu.au>
2019-05-10 17:51:23 +1000

    new feature/connect me page

---
+++ Week9Tutorial/ConnectPage/FilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FilterViewController.swift
+//  Week9Tutorial
+//
+//  Created by Ziyi Deng on 10/5/19.
+//  Copyright © 2019 Jason Haasz. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Week9Tutorial/MapPage/Place.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Place.swift
-//  Week9Tutorial
-//
-//  Created by Ziyi Deng on 7/5/19.
-//  Copyright © 2019 Jason Haasz. All rights reserved.
-//
-
-import Foundation
